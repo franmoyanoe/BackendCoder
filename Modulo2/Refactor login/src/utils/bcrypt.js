a333d6c98525a73ac7e19474f4413feca75ed57c
franmoyano <francoemmanuelmoyano@gmail.com>
2023-10-04 15:51:58 -0300

    Refactor login v3

--- Modulo2/Refactor login/src/utils/bcrypt.js
+++ Modulo2/Refactor login/src/utils/bcrypt.js
@@ -3,10 +3,10 @@ import bcrypt from 'bcrypt'
 
 //Encriptar contraseña
 export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)))
-//const hashPassword = createHash("Nico")
+//const hashPassword = createHash("Franco")
 //console.log("La contraseña encriptada es: ",hashPassword)
 
 
 //Validar contraseña encriptada
 export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD)
-//console.log("Validamos si la contraseña es correcta: ",validatePassword("Nico",hashPassword))
+//console.log("Validamos si la contraseña es correcta: ",validatePassword("Franco",hashPassword))
